ad96fa5db6d624e4bd3bf0506b114ccd5cdeecdc
tri613 <tri613@gmail.com>
2016-07-07 17:30:45 +0800

    form validation with bootstrap style

--- js/app.js
+++ js/app.js
@@ -4,49 +4,84 @@
 
 	angular.module('HoneyCake',['ngRoute'])
 
-		// .config(function($routeProvider){
-		// 	$routeProvider
-		// 		.when('/',{
-		// 			templateUrl: 'main.html',
-		// 		})
-		// 		.when('/step1',{
-		// 			templateUrl: 'step1.html',
-		// 			controller: 'ItemController'
-		// 		})
-		// 		.when('/step2',{
-		// 			templateUrl: 'step2.html',
-		// 			controller: 'DeliveryController'
-		// 		})
-		// 		.when('/finish',{
-		// 			templateUrl: 'finish.html',
-		// 			controller: 'FinishController'
-		// 		})
-		// 		.otherwise('/');
-		// })
+		.config(function($routeProvider){
+			$routeProvider
+				// .when('/',{
+				// 	templateUrl: 'main.html',
+				// })
+				.when('/step1',{
+					templateUrl: 'step1.html',
+					controller: 'ItemController'
+				})
+				.when('/step2',{
+					templateUrl: 'step2.html',
+					controller: 'DeliveryController'
+				})
+				.when('/confirm',{
+					templateUrl: 'confirm.html',
+					controller: 'ConfirmController'
+				})
+				.when('/finish',{
+					templateUrl: 'finish.html',
+					controller: 'FinishController'
+				})
+				.otherwise('/step1');
+		})
 		
 		.controller('MainController',function(){
 			
 		})
 
-		.controller('ItemController',['$scope','CartFactory',function($scope,CartFactory){
+		.controller('ItemController',['$scope','$location','CartFactory',function($scope,$location,CartFactory){
 			$scope.items = CartFactory.items;
+
+			$scope.checkValid = function(){
+				var total = CartFactory.getItemTotal();
+				if(total <= 0){
+					angular.element(document.querySelector('.alert')).removeClass('hidden-xs-up');
+					return false;
+				}
+
+				$location.url('/step2');
+			}
 			
 		}])
 
-		.controller('ConfirmController',['$scope','CartFactory',function($scope,CartFactory){
+		.controller('DeliveryController',['$scope','$location','CartFactory',function($scope,$location,CartFactory){
 			$scope.items = CartFactory.items;
+			$scope.user = {};
 
-			$scope.CalTotal = function(){
-				return CartFactory.getTotal();
-			};
+			//if no item chosen, redirect to step1
+			var total = CartFactory.getItemTotal();
+			if(total <= 0){
+				$location.url('/step1');
+			}
 
-			$scope.total = CartFactory.getTotal();
+			$scope.submitForm = function(){
+				console.log($scope.user);
+				CartFactory.setUser($scope.user);
+				// $location.url('/confirm');
+			}
+
+			// $scope.CalDilvery = function(){
+			// 	return ($scope.user.by == 'shipping') ? 150 : 0 ;
+			// }
+
+			// $scope.CalTotal = function(){
+			// 	var sum = CartFactory.getTotal();
+			// 	sum += $scope.CalDilvery();
+
+			// 	return sum;
+			// };
+
+			// $scope.total = CartFactory.getTotal();
 
 		}])
 
 		.factory('CartFactory', function(){
 			var cartFac = {};
 
+			cartFac.user = {};
 			cartFac.items = [
 				{
 					_id:0,
@@ -78,8 +113,13 @@
 			cartFac.getItem = function(_id){
 				return this.items[_id];
 			}
-
-			cartFac.getTotal = function(){
+			cartFac.setUser = function(info){
+				// if(info.by == 'facetoface'){
+				// 	info.ship = {};
+				// }
+				this.user = info;
+			}
+			cartFac.getItemTotal = function(){
 				var sum = 0;
 
 				this.items.forEach(item => {
@@ -92,6 +132,19 @@
 			}
 
 			return cartFac;
+		})
+
+		.directive("dynamicName",function($compile){
+		    return {
+		        restrict:"A",
+		        terminal:true,
+		        priority:1000,
+		        link:function(scope,element,attrs){
+		            element.attr('name', scope.$eval(attrs.dynamicName));
+		            element.removeAttr("dynamic-name");
+		            $compile(element)(scope);
+		        }
+		    };
 		})
 
 
